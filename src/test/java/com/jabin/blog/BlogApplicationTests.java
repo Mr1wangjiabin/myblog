43ae005145e20093278c4f2067f6ae6ba5316327
Mr1wangjiabin <1044704184@qq.com>
2020-08-03 22:37:47 +0800

    1、项目初始化

---
+++ src/test/java/com/jabin/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabin.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
